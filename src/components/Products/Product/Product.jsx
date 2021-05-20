5da98b14ea0796cbde32ec54a8c9e49167e06426
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-05-21 06:39:25 +0700

    Fix Grid display & add mockup images to products

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  IconButton,
+} from '@material-ui/core'
+import { AddShoppingCart } from '@material-ui/icons'
+
+import useStyles from './styles'
+
+const Product = ({ product }) => {
+  const classes = useStyles()
+
+  return (
+    <div>
+      <Card className={classes.root}>
+        <CardMedia
+          className={classes.media}
+          image={product.image}
+          title={product.name}
+        />
+        <CardContent>
+          <div className={classes.CardContent}>
+            <Typography variant="h5" gutterBottom>
+              {product.name}
+            </Typography>
+            <Typography variant="h5" gutterBottom>
+              {product.price}
+            </Typography>
+          </div>
+          <Typography variant="body2" color="textSecondary">
+            {product.description}
+          </Typography>
+          <CardActions disableSpacing className={classes.cardActions}>
+            <IconButton aria-label="Add to cart">
+              <AddShoppingCart />
+            </IconButton>
+          </CardActions>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
+
+export default Product
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Grid } from '@material-ui/core'
+import Product from './Product/Product'
+
+const products = [
+  {
+    id: 1,
+    name: 'Shoes',
+    description: 'Running Shoes.',
+    price: '$5',
+    image:
+      'https://images.unsplash.com/photo-1469395446868-fb6a048d5ca3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1190&q=80',
+  },
+  {
+    id: 2,
+    name: 'Macbook',
+    description: 'Apple Macbook',
+    price: '$10',
+    image:
+      'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80',
+  },
+]
+
+const Products = () => {
+  return (
+    <main>
+      <Grid container justify="center" spacing={4}>
+        {/* eslint-disable-next-line */}
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  )
+}
+
+export default Products
