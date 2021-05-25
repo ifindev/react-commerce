fc8189e2756234a5b8ae98d72d7dfc2c13eadea9
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-05-25 12:57:06 +0700

    Put public key openly

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -2,6 +2,6 @@ import Commerce from '@chec/commerce.js'
 require('dotenv').config()
 
 export const commerce = new Commerce(
-  process.env.REACT_APP_CHEC_PUBLIC_KEY,
+  'pk_27910d6a7a5b88f4ae2123ccfa406104ac4801db895f8',
   true
 )
