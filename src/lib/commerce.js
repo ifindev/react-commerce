4dc51ed91a4949ba2a2298c6c105007e802ae9ba
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-05-25 13:02:59 +0700

    Use process.env for API key

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,7 +1,6 @@
 import Commerce from '@chec/commerce.js'
-require('dotenv').config()
 
 export const commerce = new Commerce(
-  'pk_27910d6a7a5b88f4ae2123ccfa406104ac4801db895f8',
+  process.env.REACT_APP_CHEC_PUBLIC_KEY,
   true
 )
