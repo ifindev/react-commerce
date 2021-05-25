e5175e7fb9a320c30a3df9ba8b213a02761e6f2d
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-05-25 09:52:13 +0700

    Add fetchCart function in App.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import { Products, Navbar } from './components'
 
 const App = () => {
   const [products, setProducts] = useState([])
+  const [cart, setCart] = useState({})
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list()
     setProducts(data)
   }
 
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve())
+  }
+
   useEffect(() => {
     fetchProducts()
+    fetchCart()
   }, [])
+
+  console.log(cart)
 
   return (
     <div>
