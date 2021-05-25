e3f81590917b3c091ee43b255b9ae2571ad22ca5
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-05-25 11:36:58 +0700

    Update cart component to handle products in cart and undefined due to fetch

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,7 +6,7 @@ import useStyles from './styles'
 const Cart = ({ cart }) => {
   const classes = useStyles()
 
-  const isEmpty = cart.line_items.length === 0
+  const isEmpty = cart.line_items !== undefined ? cart.line_items === 0 : false
 
   const EmptyCart = () => (
     <Typography variant="subtitle1">
@@ -16,18 +16,46 @@ const Cart = ({ cart }) => {
 
   console.log(cart)
 
-  const FilledCart = () => (
-    <>
-      <Grid container spacing={3}>
-        {cart.line_items.map((item) => (
-          <Grid item xs={12} sm={4} key={item.id}>
-            {/* <CartItem /> */}
-            <div>{item.name}</div>
-          </Grid>
-        ))}
-      </Grid>
-    </>
-  )
+  const FilledCart = () =>
+    !isEmpty && (
+      <>
+        <Grid container spacing={3}>
+          {cart.line_items.map((item) => (
+            <Grid item xs={12} sm={4} key={item.id}>
+              {/* <CartItem /> */}
+              <div>{item.name}</div>
+            </Grid>
+          ))}
+        </Grid>
+        <div className={classes.cardDetails}>
+          <Typography variant="h4">
+            Subtotal: {cart.subtotal.formatted_with_symbol}
+          </Typography>
+          <div>
+            <Button
+              className={classes.emptyButton}
+              size="large"
+              type="button"
+              variant="contained"
+              color="secondary"
+            >
+              Empty cart
+            </Button>
+            <Button
+              className={classes.checkoutButton}
+              size="large"
+              type="button"
+              variant="contained"
+              color="primary"
+            >
+              Checkout
+            </Button>
+          </div>
+        </div>
+      </>
+    )
+
+  if (!cart.line_items) return <div>Loading...</div>
 
   return (
     <div>
