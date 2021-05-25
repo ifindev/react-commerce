88424b769051db1f45062ab6929a64a6ec88c54f
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-05-25 12:02:44 +0700

    Finishing up markup for CartItem component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Container, Typography, Button, Grid } from '@material-ui/core'
 
 import useStyles from './styles'
+import CartItem from './CartItem/CartItem'
 
 const Cart = ({ cart }) => {
   const classes = useStyles()
@@ -22,8 +23,7 @@ const Cart = ({ cart }) => {
         <Grid container spacing={3}>
           {cart.line_items.map((item) => (
             <Grid item xs={12} sm={4} key={item.id}>
-              {/* <CartItem /> */}
-              <div>{item.name}</div>
+              <CartItem item={item} />
             </Grid>
           ))}
         </Grid>
@@ -70,6 +70,7 @@ const Cart = ({ cart }) => {
         </Typography>
         <div style={{ marginTop: '20px' }} />
         {isEmpty ? <EmptyCart /> : <FilledCart />}
+        <div className={classes.toolbar} />
       </Container>
     </div>
   )
--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -6,12 +6,51 @@ import {
   CardActions,
   CardContent,
   CardMedia,
+  IconButton,
 } from '@material-ui/core'
+import { AddCircle, RemoveCircle } from '@material-ui/icons'
 
 import useStyles from './styles'
 
-const CartItem = () => {
-  return <div></div>
+const CartItem = ({ item }) => {
+  const classes = useStyles()
+  return (
+    <div>
+      <Card>
+        <CardMedia
+          image={item.media.source}
+          alt={item.name}
+          className={classes.media}
+        />
+        <CardContent className={classes.cardContent}>
+          <Typography variant="h4">{item.name}</Typography>
+          <Typography variant="h5">
+            {item.line_total.formatted_with_symbol}
+          </Typography>
+        </CardContent>
+        <CardActions className={classes.CardActions}>
+          <div className={classes.buttons}>
+            {/* <Button type="button" size="small">
+              -
+            </Button>
+            <Button type="button" size="small">
+              +
+            </Button> */}
+            <IconButton>
+              <RemoveCircle color="secondary" />
+            </IconButton>
+            <Typography>{item.quantity}</Typography>
+            <IconButton>
+              <AddCircle color="primary" />
+            </IconButton>
+          </div>
+          <Button variant="contained" type="button" color="secondary">
+            Remove
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
+  )
 }
 
 export default CartItem
