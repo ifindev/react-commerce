16ddefeb90a6850a7f78b6b38cb80018a871b6c7
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-05-27 15:13:17 +0700

    Hide cart icon on /cart route location

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,13 +9,15 @@ import {
   Typography,
 } from '@material-ui/core'
 import { ShoppingCart } from '@material-ui/icons'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 import logo from '../../assets/commerce.png'
 import useStyles from './styles'
 
 const Navbar = ({ totalItems }) => {
   const classes = useStyles()
+  const location = useLocation()
+
   return (
     <>
       <AppBar position="fixed" className={classes.appBar} color="inherit">
@@ -35,18 +37,20 @@ const Navbar = ({ totalItems }) => {
             />
           </Typography>
           <div className={classes.grow} />
-          <div className={classes.button}>
-            <IconButton
-              component={Link}
-              to="/cart"
-              aria-label="Show cart items"
-              color="inherit"
-            >
-              <Badge badgeContent={totalItems} color="secondary">
-                <ShoppingCart />
-              </Badge>
-            </IconButton>
-          </div>
+          {location.pathname !== '/cart' && (
+            <div className={classes.button}>
+              <IconButton
+                component={Link}
+                to="/cart"
+                aria-label="Show cart items"
+                color="inherit"
+              >
+                <Badge badgeContent={totalItems} color="secondary">
+                  <ShoppingCart />
+                </Badge>
+              </IconButton>
+            </div>
+          )}
         </Toolbar>
       </AppBar>
     </>
