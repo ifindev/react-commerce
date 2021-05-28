20e619476cd7a02df2c4ff8bbd622180b4695c04
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-05-28 09:48:06 +0700

    Add checkout page

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -57,6 +57,8 @@ const Cart = ({
             Empty cart
           </Button>
           <Button
+            component={Link}
+            to="/checkout"
             className={classes.checkoutButton}
             size="large"
             type="button"
